fd91ab15c6a15ad1b45de40825d6ff0a9cef8561
lootte <deede3771@gmail.com>
2025-12-14 15:40:56 -0500

    Update Pursuit.cpp

--- src/game/features/self/Pursuit.cpp
+++ src/game/features/self/Pursuit.cpp
@@ -1,1 +1,15 @@
-
+#include "core/commands/Command.hpp"
+#include "game/backend/Self.hpp"
+#include "game/rdr/Natives.hpp"
+namespace YimMenu::Features
+{
+	class Pursuit : public Command
+	{
+		using Command::Command;
+		virtual void OnCall() override
+		{
+			LAW::_SET_BOUNTY_HUNTER_PURSUIT_CLEARED();
+		}
+	};
+	static Pursuit _Pursuit{"endPursuit", "End Pursuit", "End current police pursuit"};
+}
